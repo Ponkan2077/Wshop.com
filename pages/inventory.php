11b8095ebb7bd63b573deb09fab3929f2c864447
Ponkan2077 <138117500+Ponkan2077@users.noreply.github.com>
2024-04-26 21:08:25 +0800

    admin sidebar done

---
+++ pages/inventory.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../assets/styles/style.css">
+    <link rel="stylesheet" type="text/css" href="../assets/styles/inventory.css">
+    <link rel="preload" as="image" href="../assets/img/logo_h.svg" width="100%">
+    <script src="https://kit.fontawesome.com/c0056d4561.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 481px)" href="../assets/styles/mobile.css">
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 482px) and (max-width: 768px)" href="../assets/styles/tablet.css">
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 769px) and (max-width: 1024px)" href="../assets/styles/laptop.css">
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 1025px) and (max-width: 1200px)" href="../assets/styles/desktop.css">
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 1201px)" href="../assets/styles/tv.css">
+    <title>Document</title>
+</head>
+<body class="dashboardBody">
+    <main class="mainDashboard">
+        <aside class="dashboardAside" style="float: left" >
+            <div class = "logoAdmin">
+                <a href="/Wshop.com/pages/dashboard.php">
+                <img src="/Wshop.com/assets/img/logo_h.svg">
+                </a>
+            </div>
+            <nav class="navAdmin">
+        <ul>
+            <li><a href="/Wshop.com/pages/dashboard.php" class="adminDashboard">Dashboard</a></li>
+            <li><a href ="#" class="adminInventory">Inventory</a></li>
+            <li><a href ="/Wshop.com/pages/order.php" class="adminOrder">Orders</a></li>
+        </ul>
+    </nav>
+    <div class="logoutAdmin">
+       <a href ="/Wshop.com/pages/login/login.php">Logout</a>
+    </div>
+        </aside>
+    </main>
+</body>
+</html>
