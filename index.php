36f52b1b39b4b2a725d4f0146125cb399082f35c
Ponkan2077 <138117500+Ponkan2077@users.noreply.github.com>
2024-04-30 21:05:47 +0800

    hm

--- index.php
+++ index.php
@@ -38,13 +38,13 @@
     <div class ="leftProduct">
         <h3>Pre-Built Systems</h3>
         <div class="boxProduct">
-            <img src="./assets/img/logo_h.svg" class="anyProduct" alt="picture">
+            <img src="./assets/img/products/desktop/desktop1/Fotoram.io.jpg" class="anyProduct" alt="picture">
         </div>
     </div>
     <div class ="rightProduct">
         <h3>Laptops</h3>
         <div class="boxProduct">
-        <img src="./assets/img/logo_h.svg" class="anyProduct" alt="picture"></div>
+        <img src="./assets/img/products/laptop/laptop 1/4479e64a-1343-444c-a202-fd470147ce63-fotor-bg-remover-202404272345.png" class="anyProduct" alt="picture"></div>
         </div>
     </div>
 </div>
@@ -55,47 +55,141 @@
     <div class ="wrapTopSellers">
         <div class ="wrapTop">
     <div class="GPUS">
-        <img src="./assets/img/logo_h.svg" class = "box" alt="picture">
+        <img src="./assets/img/products/gpu/nvidia-geforce-graphics-card-gpus-wallpaper-preview-fotor-bg-remover-2024042731432.png" class = "box" alt="picture">
         <h4>GPUS</h4>
     </div>
     <div class="CPUS">
-    <img src="./assets/img/logo_h.svg"  class="box" alt="picture">
+    <img src="./assets/img/products/cpu/cpu 2/AMD-Ryzen-9-7900X3D-gaming-processor.jpg"  class="box" alt="picture">
         <h4>CPUS</h4>
     </div>
 </div>
 <div class="wrapDown">
     <div class="RAMS">
-    <img src="./assets/img/logo_h.svg"  class="box" alt="picture">
+    <img src="./assets/img/products/ram/ram 1/adata_8gb_ddr5_4800_40_8gx4_so-dimm_ad5s48008g-s_-fotor-bg-remover-202404273487.png"  class="box" alt="picture">
         <h4>Rams</h4>
     </div>
     <div class="MOTHERBOARD">
-    <img src="./assets/img/logo_h.svg"  class="box" alt="picture">
+    <img src="./assets/img/products/motherboard/mother board 1/mb-box2.png"  class="box" alt="picture">
         <h4>Motherboard</h4>
     </div>
 </div>
     </div>
 </section>
 
-<section class="reviews">
-    <h3>Reviews</h3>
-    <div class="desktopScreen">
-    <div class="wrapBoxReview">
-    <img src="./assets/img/logo_h.svg"   class="boxReview" alt="picture">
-    <img src="./assets/img/logo_h.svg" class="boxReview" alt="picture">
-    <img src="./assets/img/logo_h.svg"  class="boxReview" alt="picture">
-</div>
-</div>
-    <div class="mobileScreen"> 
-    <img src="./assets/img/logo_h.svg"  class="boxReview" alt="picture">
+
+<div id="reviewsContainer">
+  <section id="reviews">
+    
+    <div class="review-heading">
+        <h1>Reviews</h1>
     </div>
+    
+    <div class="review-box-container">
+        
+        <div class="review-box">
+            <div class="box-top">
+                <div class="profile">
+                    <div class="profile-img">
+                        <img src="/Wshop.com/assets/img/profilepic.webp" />
+                    </div>
+                    <div class="name-user">
+                        <strong>Abdul-Rey Flores</strong>
+                        <span>@princessrey</span>
+                    </div>
+                </div>
+                <div class="reviews">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="far fa-star"></i>
+                </div>
+            </div>
+            <div class="client-comment">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident
+                    temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur
+                    voluptates incidunt blanditiis sed atque cumque.</p>
+            </div>
+        </div>
+        <div class="review-box">
+            <div class="box-top">
+                <div class="profile">
+                    <div class="profile-img">
+                        <img src="/Wshop.com/assets/img/profilepic.webp" />
+                    </div>
+                    <div class="name-user">
+                        <strong>J.D. Labastillas</strong>
+                        <span>@jdminimixko</span>
+                    </div>
+                </div>
+                <div class="reviews">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+            </div>
+            <div class="client-comment">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident
+                    temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur
+                    voluptates incidunt blanditiis sed atque cumque.</p>
+            </div>
+        </div>
+        <div class="review-box">
+            <div class="box-top">
+                <div class="profile">
+                    <div class="profile-img">
+                        <img src="/Wshop.com/assets/img/profilepic.webp"/>
+                    </div>
+                    <div class="name-user">
+                        <strong>Jamal</strong>
+                        <span>@pogiako</span>
+                    </div>
+                </div>
+                <div class="reviews">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="far fa-star"></i>
+                </div>
+            </div>
+            <div class="client-comment">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident
+                    temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur
+                    voluptates incidunt blanditiis sed atque cumque.</p>
+            </div>
+        </div>
+        <div class="review-box">
+            <div class="box-top">
+                <div class="profile">
+                    <div class="profile-img">
+                        <img src="/Wshop.com/assets/img/profilepic.webp" />
+                    </div>
+                    <div class="name-user">
+                        <strong>Fatima Kijar</strong>
+                        <span>@retired</span>
+                    </div>
+                </div>
+                <div class="reviews">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="far fa-star"></i><!--Empty star-->
+                </div>
+            </div>
+            <div class="client-comment">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident
+                    temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur
+                    voluptates incidunt blanditiis sed atque cumque.</p>
+            </div>
+        </div>
     </div>
-    <div class="tabletScreen"> 
-    <img src="./assets/img/logo_h.svg" class="boxReview" alt="picture">
-    </div>
-    <div class="laptopScreen"> 
-    <img src="./assets/img/logo_h.svg" class="boxReview" alt="picture">
-    </div>
-</section>
+   </section>
+  </div>
+
 
 
 <section class="payment">
